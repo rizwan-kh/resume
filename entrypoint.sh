2604c37b6e671c563323eaf20d2174d635d77f2a
Rizwan Khan <rizwan.kh@accenture.com>
2021-06-01 13:48:40 +0400

    corrected path for theme.tgz

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ cd ${WORKDIR}
 # retrieve and install jsonresume theme
 wget "$1" -O /theme.tgz
 npm install /theme.tgz
-package_name=$(npm pack /theme.tgz --dry-run 2>&1 | grep "^npm notice name:" | sed 's/npm notice name:\s*\(.*\)/\1/')
+package_name=$(npm pack /theme.tgz --dry-run 2>&1 | grep "^npm notice name:" | sed 's/npm notice name:\([ ]*\)\(.*\)/\2/')
 
 # export to HTML
 resume export -t ./node_modules/$package_name resume.html
